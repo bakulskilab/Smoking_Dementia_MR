dd9f021bdb2e59aa07bf0399f66edef89ec1d94a
Joy <mzfu@umich.edu>
2023-01-24 22:06:32 -0800

    1/24/2023 updates

--- extract_snps.sh
+++ extract_snps.sh
@@ -7,12 +7,12 @@ geno_file=chrALL
 genorn=${geno_path}${geno_file}
 
 sumstats_path=/Users/Mingzhou/Desktop/AD_Grant/smk_dem_MR/data/sumstats/sig_snps/
-finalsnplist=${sumstats_path}candidate_SC_wdup.txt
+finalsnplist=${sumstats_path}candidate_AD_wdup.txt
 
-# candidate_SI_wdup.txt candidate_CPD_wdup.txt  candidate_AI_wdup.txt
+# candidate_SI_wdup.txt candidate_CPD_wdup.txt  candidate_AI_wdup.txt candidate_SC_wdup.txt
 
 #====== Extract HRS =======
 genout_path=/Users/Mingzhou/Desktop/AD_Grant/smk_dem_MR/data/genome/
-genoex=${genout_path}${geno_file}.indsig.SC
+genoex=${genout_path}${geno_file}.indsig.AD
 
 $plink_call --noweb --allow-no-sex --bfile ${genorn} --extract ${finalsnplist} --make-bed --out ${genoex}
